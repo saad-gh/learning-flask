b0623f1e73fc2017b45ad012b48c810208381a76
Saad Khan <s.aad@live.com>
2018-08-31 10:04:11 +0000

    added constants

--- __init__.py
+++ __init__.py
@@ -4,11 +4,15 @@ from flask import Flask
 
 
 def create_app(test_config=None):
+    # constants
+    SECRET_KEY = 'dev'
+    DB_NAME = 'learning'
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'learning.sqlite'),
+        SECRET_KEY=SECRET_KEY,
+        DATABASE=os.path.join(app.instance_path, DB_NAME + '.sqlite'),
     )
 
     if test_config is None:
@@ -24,7 +28,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
+    # a simple page that says Salam
     @app.route('/salam')
     def hello():
         return 'Salam, World!'
